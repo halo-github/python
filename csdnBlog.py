4fc1a657a48bca9c76f4773429cbf74a72b2bffe
halo-github <Fenix@vvhalodeiMac.local>
2016-05-31 17:46:18 +0800

    修改正则表达式

--- csdnBlog.py
+++ csdnBlog.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #coding: UTF-8
 urlStr = "http://blog.csdn.net/"
-import urllib2,threading
+import requests,threading,re
 class MyClass(threading.Thread):
 	def __init__(self,thread_name,url):
 		threading.Thread.__init__(self)
@@ -9,13 +9,34 @@ class MyClass(threading.Thread):
 		self.url = url
 
 	def run(self):
-		req = urllib2.Request(self.url)
-		response = urllib2.urlopen(req)
-		data = response.read()
-		fileHandle = open("csdnBlog.rtf","w")
-		fileHandle.write(data)
+		try:
+			req = requests.get(self.url,timeout = 1)
+			data = req.text
+		#	print data
+			self.findUrl(data,0)
+			#fileHandle = open("csdnBlog.rtf","w")
+			#fileHandle.write(data)
+		except :
+			print "type error"
+		finally:
+			print "ok"
+		
+	def findUrl(self,html,depth):	
+		if (depth == 0):
+			reStr1 = r'"http.+?"'
+			reStr2 = r'"(/?[^\s<>]+?/[^\s<>]+?)"'
 
+#			comp1 = re.compile(reStr1)
+#			list1 = re.findall(comp1,html)
 
+			comp2 = re.compile(reStr2)
+			list2 = re.findall(comp2,html)
+			list2.sort()
+			for s2 in list2:
+				print s2
+			#	r = requests.get(s,timeout=1)
+
+			
 if __name__ == '__main__':
 	urlSite = raw_input("设置入口网址\n")
 	id = MyClass("hello",urlSite)
