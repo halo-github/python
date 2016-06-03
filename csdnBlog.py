61cf9d8e5d58408ce2d9e0639379fd06ce8392f0
halo-github <Fenix@vvhalodeiMac.local>
2016-06-03 17:57:05 +0800

    threading.Lock()
    lock.acquire()
    lock.release()

--- csdnBlog.py
+++ csdnBlog.py
@@ -3,62 +3,69 @@
 urlStr = "http://blog.csdn.net/"
 import requests,threading,re,Queue
 class MyClass(threading.Thread):
-	def __init__(self,url,queue):
+	def __init__(self,queue,lock):
 		threading.Thread.__init__(self)
-		self.url = url
 		self.queue = queue
+		self.lock = lock
 
 	def run(self):
-		global mutex
-		file = open("csdn.rtf","wb")
-		try:	
-			siteList = file.read()
-			if siteList:
-				global x,mutex
-			#	mutex.acquire()
-				print siteList[x]
-				x = x+1
-				print x
-			#	mutex.release()
-				
-			else :
-				req = requests.get(self.url,timeout = 1)
+	  try:	
+	    while 1:	
+		if self.queue.qsize()>0:
+			if self.lock.acquire(1):
+				print "加锁1"
+				if self.queue.qsize()==0:
+					print "解锁1"
+					self.lock.release()	
+				url = self.queue.get()
+				print self.name + " get url from queue"
+				self.lock.release()	
+				print "解锁1"
+				print url
+				req = requests.get(url,timeout = 10)
 				data = req.text
-				self.findUrl(data,0)
-				file.write(siteList)
-				
-		except :
-			print "type error"
+				print self.name + " is running"
+				self.findUrl(url,data)
+  	  except :
+	  	  pass
+	  finally:
+		  print "ok"
+
+	def findUrl(self,url,html):
+		print "find url start..."
+		siteList = []
+		global mutex
+		try:
+			host = url.split("/")[2]
+			reStr1 = r'"(/[^\s<>/]+?/[^\s<>]+?)"'
+			reStr2 = r'"(http.+?)(!.js)"'
+			comp1 = re.compile(reStr1)
+			comp2 = re.compile(reStr2)
+			list1 = re.findall(comp1,html)
+			list2 = re.findall(comp2,html)
+			list3 = []
+			for s in list1:
+				list3.append(host+s)
+			siteList = list2 + list3		
+			print  self.lock
+			if self.lock.acquire(1):
+				print "加锁2"
+				for i in siteList:
+					self.queue.put(i)
+				self.lock.release()	
+				print "解锁2"
+			print self.queue.qsize()
 		finally:
-			file.close()	
-	def findUrl(self,html,depth):
-		global siteList	
-		host = self.url.split("/")[2]
-		print host
-		reStr1 = r'"(/[^\s<>/]+?/[^\s<>]+?)"'
-		reStr2 = r'"(http.+?)"'
-		comp1 = re.compile(reStr1)
-		comp2 = re.compile(reStr2)
-		list1 = re.findall(comp1,html)
-		list2 = re.findall(comp2,html)
-		list3 = []
-		for s in list1:
-			list3.append(host+s)
-		siteList = list2 + list3
+			print
+		
 def main():			
-	global siteList
+	global siteList,mutex
 	mutex = threading.Lock()
-	queue = Queue.Queue()	
-	siteList = []
+	queue = Queue.Queue()
 	urlSite = raw_input("设置入口网址\n")
-	id = MyClass(urlSite,queue)
-	id.start()		
-	id.join()
-	
-	for i in siteList:
-		id = MyClass(i,queue)
-		id.start()	
-		id.join()
-x = 0
+	queue.put(urlSite)
+	threads = [MyClass(queue,mutex) for x in xrange(10)]
+	for t in threads:
+		t.start()
 if __name__ == '__main__':
 	main()
